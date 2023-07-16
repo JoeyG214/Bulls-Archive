25576d794fede137d9e25014bcc02529ab3b21d5
JoeyG214 <erjo.guce214@gmail.com>
2023-07-15 22:25:26 -0400

    Styling changes to Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,25 +4,27 @@ import Image from "next/image"
 const Nav = () => {
   return (
     <header className="absolute inset-x-0 top-0 z-50">
-      <nav className="flex items-center justify-between p-2 lg:px-8">
-        <div>
-          <Link href={'/'} className="-m-1.5 p-1.5">
-            <Image
-              className="h-8 w-auto sm:h-12"
-              src="/usf-logo.png"
-              alt="USF Logo"
-              width={128}
-              height={128}
-            />
-          </Link>
+      <nav className="flex items-center justify-between p-4 lg:px-8">
+        <div className="flex items-center gap-2 md:gap-6">
+          <div className="h-10 sm:h-14 w-auto">
+            <Link href={'/'}>
+              <Image
+                className="h-8 sm:h-12 w-auto"
+                src="/usf-logo.png"
+                alt="USF Logo"
+                width={100}
+                height={100}
+              />
+            </Link>
+          </div>
+          <div>
+            <h1 className="flex text-gray-900 text-lg xs:text-2xl font-semibold tracking-widest">
+              Bulls Archive
+            </h1>
+          </div>
         </div>
         <div>
-          <h1 className="flex text-gray-900 text-lg xs:text-2xl font-semibold tracking-widest">
-            Bulls Archive
-          </h1>
-        </div>
-        <div>
-          <Link href="/login" className="text-sm lg:text-lg text-gray-900 hover:text-green-600 font-semibold leading-6">
+          <Link href="/login" className="text-md md:text-lg text-gray-900 hover:text-green-600 font-semibold leading-6">
             Log in
           </Link>
         </div>
