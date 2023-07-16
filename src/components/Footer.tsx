9b52700e037180202ea81f88c895c1398e7aedd9
JoeyG214 <erjo.guce214@gmail.com>
2023-07-15 23:01:21 -0400

    Update footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <footer className="bg-primary shadow">
+    <footer className="flex shadow-lime-600 shadow">
       <div className="flex justify-center p-4 mx-auto">
-        <span className="text-sm md:text-lg text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="text-sm md:text-lg text-gray-500 font-semibold text-center">
           © {new Date().getFullYear()} <Link href={'https://github.com/JoeyG214'} className="hover:underline">Joey Guce</Link>. All Rights Reserved.
         </span>
       </div>
