121d35da24c0993bf35dd5d92c08792ba45feebb
JoeyG214 <erjo.guce214@gmail.com>
2023-07-22 04:21:07 -0400

    Provide auth session to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google'
 
 import Nav from '@/components/Nav'
 import Footer from '@/components/Footer'
+import AuthProvider from '@/components/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,7 @@ export const metadata = {
   description: 'Discover and Share Computer Science Notes',
 }
 
-export default function RootLayout({
+function RootLayout({
   children,
 }: {
   children: React.ReactNode
@@ -21,12 +22,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="flex flex-col justify-between min-h-screen bg-white">
-          <Nav />
-          {children}
-          <Footer />
-        </main>
+        <AuthProvider>
+          <main className="flex flex-col justify-between min-h-screen bg-white">
+            <Nav />
+            {children}
+            <Footer />
+          </main>
+        </AuthProvider>
       </body>
     </html>
   )
 }
+
+export default RootLayout
